fe863bb8b8b4027e79e09f9260239b8c0125a1b6
enuii3 <b.nxxxi551@gmail.com>
2023-02-27 08:29:55 +0900

    vuetifyの設定

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,21 +2,21 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import colors from "vuetify/lib/util/colors";
+// import colors from "vuetify/lib/util/colors";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives, // primaryの参考文献 https://vuetifyjs.com/en/styles/colors/
-    themes: {
-      light: {
-        dark: false,
-        colors: {
-          primary: colors.blue.lighten3,
-          secondary: colors.green.lighten3,
-        },
-      },
-    },
+    // themes: {
+    //   light: {
+    //     dark: false,
+    //     colors: {
+    //       primary: colors.blue.lighten3,
+    //       secondary: colors.green.lighten3,
+    //     },
+    //   },
+    // },
     icons: {
       defaultSet: "mdi",
     },
