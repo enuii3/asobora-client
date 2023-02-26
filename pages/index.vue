a7f17d4be39a09ccca661b8de0cc2507b36a42a5
enuii3 <b.nxxxi551@gmail.com>
2023-02-26 19:35:01 +0900

    index.vueの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+import { headerMenu } from "~~/src/utils/list";
+</script>
+
 <template>
-  <div>main page</div>
+  <div>
+    <img
+      class="top-pic"
+      src="~/assets/images/asobora-top.jpg"
+      alt="asobora-top"
+    />
+    <ul v-for="(menu, i) in headerMenu" :key="i">
+      <v-card
+        v-if="menu.title !== 'Contact'"
+        class="index-card"
+        :title="menu.title"
+      />
+    </ul>
+  </div>
 </template>
+
+<style scoped>
+.top-pic {
+  width: 100%;
+}
+
+.index-card {
+  margin: 20px;
+}
+</style>
